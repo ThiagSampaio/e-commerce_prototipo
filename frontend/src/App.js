06c6900eac834fea7b5b5540f57c4d593a59362b
Thiago Sampaio <thiagosampaioparticular1@gmail.com>
2021-12-06 16:55:49 -0300

    Tela principal listagem de produtos

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,15 @@ import { Container } from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
 
+import HomeScreen from './screens/HomeScreen';
+
 function App() {
   return (
     <div>
       <Header />
       <main className="py-3">
         <Container>
+          <HomeScreen />
           <h1>Welcome</h1>
         </Container>
       </main>
