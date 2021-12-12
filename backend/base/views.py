cac5bd98465dc2f56ae846932c137147c7fb4231
Thiago Sampaio <thiagosampaioparticular1@gmail.com>
2021-12-12 00:05:50 -0300

    Protegendo rotas

--- backend/base/views.py
+++ backend/base/views.py
@@ -1,9 +1,11 @@
 from django.contrib.auth.models import update_last_login
 from django.shortcuts import render
 from django.http import JsonResponse
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 
+from django.contrib.auth.models import User
 
 from .models import Product
 from .products import products
@@ -37,10 +39,18 @@ def getRoutes(request):
 
 
 @api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def getUserProfile(request):
     user = request.user
-
     serializer = UserSerializer(user, many=False)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+@permission_classes([IsAdminUser])
+def getUsers(request):
+    users = User.objects.all()
+    serializer = UserSerializer(users, many=True)
     return Response(serializer.data)
 
 
